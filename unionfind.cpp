c64d0694d8e48d626f62ecfa237be9cbfc095f9c
hongzicong <973138379@qq.com>
2017-12-26 23:52:11 +0800

    20171226
    连通问题（使用union-find解决问题）

---
+++ unionfind.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+using namespace std;
+
+int id[100001];
+
+int find(int a);
+void union_me(int a,int b);
+bool connected(int a,int b);
+
+int main(int argc, char const *argv[]){
+
+  int a,b;
+
+  for(int i=0;i<100001;++i){
+    id[i]=i;
+  }
+
+  while(cin>>a>>b){
+    if(!connected(a,b)){
+      cout<<a<<" "<<b<<endl;
+    }
+    union_me(a,b);
+  }
+
+  return 0;
+}
+
+int find(int a){
+  while(a!=id[a]){
+    a=id[a];
+  }
+  return a;
+}
+
+void union_me(int a,int b){
+  int aRoot=find(a);
+  int bRoot=find(b);
+  if(aRoot==bRoot){
+    return;
+  }
+  id[aRoot]=bRoot;
+}
+
+bool connected(int a,int b){
+  return find(a)==find(b);
+}
